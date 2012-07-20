3656c9030b85ae8063fc2761b4e2daa0f9e3abbe
ruwhan <ruwhan@windowslive.com>
2012-07-20 20:54:43 +0700

    issue #12 add all new fresh files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogLearning::Application.config.secret_token = 'fb9dc5a981124e10568ed09f94f84ce6e298130189ada4b02feba961f9975a0e81429705ac8a4083e1da42395d1cb4e532308c289e3ca4177613ac366cf80c55'
