1732e72c53b5c92fd3cc45c566d2f15d5edfc0fc
ruwhan <ruwhan@windowslive.com>
2012-07-21 09:13:36 +0700

    issue #15 implement blog post and comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   attr_accessible :comment, :name
+  
+  validates :name, :presence => true
+  validates :comment, :presence => true, :length => { :within => 3..1000 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   attr_accessible :content, :created_at, :last_updated_at, :title
+  
+  validates :title, :presence => true, :length => { :within => 3..100 }
+  validates :content, :presence => true,
+                      :length => { :minimum => 10 }
 end
