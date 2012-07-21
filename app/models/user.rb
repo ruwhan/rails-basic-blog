72207f59465755348055bb2744c04d82f08965fc
ruwhan <ruwhan@windowslive.com>
2012-07-21 15:17:49 +0700

    issue #19 encrypt user password in the db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
+require 'digest'
+
 class User < ActiveRecord::Base
   has_many :posts
+  has_many :replies, :through => :posts, :source => :comments
   
   attr_accessible :email, :password, :password_confirmation
   validates :email, :presence => true,
@@ -8,4 +11,16 @@ class User < ActiveRecord::Base
   validates :password, :confirmation => true,
                         :presence => true,
                         :length => { :within => 6..35 }
+
+  before_save :encrypt_new_password
+  
+  protected
+    def encrypt_new_password
+      return if password.blank?
+      self.password = encrypt(password)
+    end
+    
+    def encrypt(string_pwd)
+      Digest::SHA1.hexdigest(string_pwd)
+    end
 end
