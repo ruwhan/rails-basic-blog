414180106018c9c6f7e412c9561a3561a3419c5e
ruwhan <ruwhan@windowslive.com>
2012-07-21 15:01:45 +0700

    issue #18 implement user account fields validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   has_many :posts
   
-  attr_accessible :email, :password, :password_confirmation  
-  validates :password, :confirmation => true
+  attr_accessible :email, :password, :password_confirmation
+  validates :email, :presence => true,
+                    :length => { :maximum => 35 },
+                    :format => { :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i }
+  validates :password, :confirmation => true,
+                        :presence => true,
+                        :length => { :within => 6..35 }
 end
