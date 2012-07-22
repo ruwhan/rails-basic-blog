ec29ed3de3bbe344b35059c5ebeb6f41cbc3c0cc
ruwhan <ruwhan@windowslive.com>
2012-07-22 07:56:04 +0700

    remove old blog project folder

--- blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '8369682989cdc87c22fd09c376ed395d8b878c469510a7b0afde45bcca46de80deae640d6987e08f2832594e86af62d30932fd96c6405af023ac199d22bccdce'
