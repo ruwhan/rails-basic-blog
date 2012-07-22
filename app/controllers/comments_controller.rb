046081e9b2fd66309952dbeae3bea0b3065b3545
ruwhan <ruwhan@windowslive.com>
2012-07-22 07:40:00 +0700

    issue #22 implement ajax for comment addition

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,28 @@
 class CommentsController < ApplicationController
   before_filter :authenticate, :only => :destroy
   
+  def new
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.new
+  end
+  
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment])
+    @comment = @post.comments.new(params[:comment])
     
-    redirect_to post_path(@post)
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to @post, :notice => 'thanks for your comment' }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @post, :alert => 'unable to add comment' }
+        format.js { render 'fail_create.js.erb' }
+      end
+    end
+    
+    #redirect_to post_path(@post)
   end
 
   def destroy
