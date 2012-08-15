871ea71dc7e3d4a3339fa2a31fffedba6a2acc5a
ruwhan <ruwhan@windowslive.com>
2012-08-16 05:25:49 +0700

    #26 change email account credentials.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ BlogLearning::Application.configure do
     :enable_starttls_auto => true,
     :port => 587,
     :authentication => :plain,
-    :user_name => '',
-    :password => ''
+    :user_name => 'whatsupp.development@gmail.com',
+    :password => 'whatsupadm1n'
   }
 end
